90cebe4bf04fe4b6725175ca6c62f6e820ef3a3b
Aleksey <kolchenkoav@gmail.com>
2024-04-18 14:08:35 +0300

    fix

---
+++ src/test/java/com/example/contactlist/repository/DatabaseContactRepositoryTest.java
@@ -0,0 +1,20 @@
+package com.example.contactlist.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseContactRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void findAll() {
+        assertThat(true).isTrue();
+    }
+}
